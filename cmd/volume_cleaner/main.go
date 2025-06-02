b75c30b7cc85b57cfdf74b22da169a4be83f980c
anrayliu <anrayliu@gmail.com>
2025-06-02 18:04:23 +0000

    feat: test for grabbing sts manifest before deletion

--- cmd/volume_cleaner/main.go
+++ cmd/volume_cleaner/main.go
@@ -2,62 +2,76 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/rest" // for InClusterConfig and RESTClient
 )
 
-type Config struct {
-	ClientID       string
-	ClientSecret   string
-	TenantID       string
-	DryRun         bool
-	AllowedDomains []string
-	GracePeriod    int
-}
-
 func main() {
-	fmt.Println("Volume cleaner started.")
-
-	cfg := Config{
-		DryRun:      os.Getenv("DRY_RUN") == "true",
-		GracePeriod: 30,
-	}
-
-	kubeClient, err := initKubeClient()
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("Error creating kube client: %v", err)
+		panic(err)
 	}
 
-	cleanVolumes(kubeClient, cfg)
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// watchPods(clientset)
+	watchSts(clientset)
+}
+
+func watchSts(clientset *kubernetes.Clientset) {
+	// Watch StatefulSets in all namespaces
+	watcher, err := clientset.AppsV1().StatefulSets("").Watch(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Watching for StatefulSet events...")
+
+	for event := range watcher.ResultChan() {
+		sts, ok := event.Object.(*appsv1.StatefulSet)
+		if !ok {
+			continue
+		}
+
+		switch event.Type {
+		case watch.Deleted:
+			fmt.Printf("StatefulSet deleted: %s/%s\n", sts.Namespace, sts.Name)
+			fmt.Println(sts)
+			dumpManifest(sts)
+		}
+	}
+
+	for event := range watcher.ResultChan() {
+		sts, ok := event.Object.(*appsv1.StatefulSet)
+		if !ok {
+			continue
+		}
+
+		switch event.Type {
+		case watch.Deleted:
+			fmt.Printf("StatefulSet deleted: %s/%s\n", sts.Namespace, sts.Name)
+			fmt.Println(sts)
+			dumpManifest(sts)
+		}
+	}
 
 }
 
-// pointers?
-
-func initKubeClient() (*kubernetes.Clientset, error) {
-	// service runs inside cluster as a pod, therefore will use in-cluster config
-	// to connect with cluster
-
-	cfg, err := rest.InClusterConfig()
-	if err == nil {
-		return kubernetes.NewForConfig(cfg)
-	}
-	return nil, err
-}
-
-func cleanVolumes(kube kubernetes.Interface, cfg Config) {
-	vols, err := kube.CoreV1().PersistentVolumes().List(context.TODO(), metav1.ListOptions{})
+func dumpManifest(sts *appsv1.StatefulSet) {
+	out, err := json.MarshalIndent(sts, "", "  ")
 	if err != nil {
-		log.Fatalf("Error listing volumes: %v", err)
+		fmt.Printf("Failed to marshal STS: %v\n", err)
+		return
 	}
-
-	for _, vol := range vols.Items {
-		fmt.Println(vol.Name, vol.Spec.ClaimRef.Name)
-	}
-
+	fmt.Println("Last known manifest before deletion:")
+	fmt.Println(string(out))
 }
