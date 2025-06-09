ab6351db10a240bc299863e190164b5db235fb8a
Justin Zhang <jing.justin@gmail.com>
2025-06-09 14:03:38 -0400

    feat(control loop): add the control loop of the watcher (#54)
    * feat(watcher): add a main control loop
    
    This change will add a main control loop that watches for statefulset
    events and triggers on sts creation or deletion
    
    * fix(packages): import missing packages
    
    The binary for the volume-cleaner was failing to compile due to unknown operators
    (OR operator) and packages such as Appsv1 and kubernetes package not
    being imported. This commit fixes that.
    
    * feat(sts pvcs): get the pvcs of a sts
    
    This change will create a function in the kubernetes/retriever package
    that given a sts, it will return its associated persistent volume
    claims, this is done using owner references, there are alternative
    methods like using the name structure of PVCs, but that makes it less
    robust, thus the owner reference method was use. Unit testing has also
    been done to test whether the function acquires the right owned pvcs and
    ignores the ones that are not "owned"
    
    * feat(sts pvc): add get pvcs of sts in control loop
    
    This adds getting the pvcs associated with an sts into the main
    control loop of the watcher
    
    * fix(remove): remove or comment variables for tests
    
    * fix(return pointer): return a slice
    
    Due to escape analysis, it is bad to return a slice pointer, this has
    been fixed now
    
    ---------
    
    Co-authored-by: Justin Zhang <justinjing.zhang@statcan.gc.ca>

---
+++ internal/kubernetes/watcher.go
@@ -0,0 +1,47 @@
+package kubernetes
+
+import (
+	// External Imports
+	"context"
+	"log"
+
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+)
+
+// Watches for when statefulsets are created or deleted across all namespaces
+func watchSts(kube *kubernetes.Clientset) {
+	watcher, err := kube.AppsV1().StatefulSets(metav1.NamespaceAll).Watch(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		log.Fatalf("Error creating a watcher for statefulsets: %v", err)
+	}
+
+	log.Print("Watching for statefulset events...")
+
+	// Watch Loop
+	events := watcher.ResultChan()
+	for event := range events {
+		sts, ok := event.Object.(*appsv1.StatefulSet)
+
+		// Skip this event if it can't be parsed into a sts
+		if !ok {
+			continue
+		}
+
+		// stsPvcs := PvcListBySts(kube, sts)
+
+		switch event.Type {
+		case watch.Added:
+			log.Printf("sts added: %s\n", sts)
+			// Call Labeler
+		case watch.Deleted:
+			log.Printf("sts deleted: %s\n", sts)
+			// Call Labeler
+		}
+
+	}
+
+}
