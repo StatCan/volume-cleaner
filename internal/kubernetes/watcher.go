b3e8caee299af8867222dcd54e5ba13ec9edc163
anrayliu <anrayliu@gmail.com>
2025-06-18 15:25:35 +0000

    refactor(controller): clean up watcher.go

--- internal/kubernetes/watcher.go
+++ internal/kubernetes/watcher.go
@@ -1,40 +1,41 @@
 package kubernetes
 
 import (
-	// External Imports
+	// standard packages
 	"context"
 	"log"
 	"time"
 
-	structInternal "volume-cleaner/internal/structure"
-
+	// external packages
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+
+	// internal packages
+	structInternal "volume-cleaner/internal/structure"
 )
 
 // Watches for when statefulsets are created or deleted across all namespaces
+
 func WatchSts(ctx context.Context, kube kubernetes.Interface, cfg structInternal.ControllerConfig) {
-	// leaving namespace as anray-liu for now until more rigorous testing is done
-	// reminder to not hard code namspace after unit tests are done
-
 	watcher, err := kube.AppsV1().StatefulSets(cfg.Namespace).Watch(ctx, metav1.ListOptions{})
-
 	if err != nil {
-		log.Fatalf("Error creating a watcher for statefulsets: %v", err)
+		log.Fatalf("Error creating a watcher for statefulsets: %s", err)
 	}
 
 	log.Print("Watching for statefulset events...")
 
-	// Watch Loop
 	events := watcher.ResultChan()
 
 	for {
 		select {
+
+		// context used to kill loop
 		case <-ctx.Done():
 			return
 
+		// sts was added or deleted
 		case event := <-events:
 			sts, ok := event.Object.(*appsv1.StatefulSet)
 
@@ -46,6 +47,7 @@ func WatchSts(ctx context.Context, kube kubernetes.Interface, cfg structInternal
 			switch event.Type {
 			case watch.Added:
 				log.Printf("sts added: %s", sts.Name)
+
 				for _, vol := range sts.Spec.Template.Spec.Volumes {
 					log.Printf("removing label")
 
@@ -53,6 +55,7 @@ func WatchSts(ctx context.Context, kube kubernetes.Interface, cfg structInternal
 				}
 			case watch.Deleted:
 				log.Printf("sts deleted: %s", sts.Name)
+
 				for _, vol := range sts.Spec.Template.Spec.Volumes {
 					log.Printf("adding label")
 
