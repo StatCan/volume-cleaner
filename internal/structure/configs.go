d7ef8b492757d615581affd422ca8051fc07a852
anrayliu <anrayliu@gmail.com>
2025-06-18 15:25:58 +0000

    refactor: clean up configs

--- internal/structure/configs.go
+++ internal/structure/configs.go
@@ -1,5 +1,23 @@
 package structure
 
+/*
+Example configs
+
+controller:
+
+NAMESPACE: "anray-liu"
+LABEL: "volume-cleaner/unattached-time"
+TIME_FORMAT: "2006-01-02_15-04-05Z"
+
+scheduler:
+
+NAMESPACE: "anray-liu"
+LABEL: "volume-cleaner/unattached-time"
+GRACE_PERIOD: "180"
+TIME_FORMAT: "2006-01-02_15-04-05Z"
+DRY_RUN: "true"
+*/
+
 type ControllerConfig struct {
 	Namespace  string
 	Label      string
