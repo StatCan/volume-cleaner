97e292a18c9abf947272b19e81f62052c832a038
Justin Zhang <jing.justin@gmail.com>
2025-06-23 20:39:53 +0000

    refactor: move defered response usage

--- internal/utils/email.go
+++ internal/utils/email.go
@@ -48,7 +48,6 @@ func SendNotif(client *http.Client, conf structInternal.EmailConfig, email strin
 
 	// Send Request
 	response, err := client.Do(request)
-	defer response.Body.Close()
 
 	if err != nil {
 		log.Printf("Error making HTTP POST request: %v", err)
@@ -56,6 +55,9 @@ func SendNotif(client *http.Client, conf structInternal.EmailConfig, email strin
 		// sending the email failed, but don't stop the program
 		return false
 	}
+
+	defer response.Body.Close()
+
 	log.Printf("Successfully Sent Email Notif to %s: %s", personal.Name, response.Status)
 
 	return response.StatusCode != 201 // return err boolean
