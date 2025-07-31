75da4af47e5845aefbb8062c9c2bf10303ba2149
anrayliu <anrayliu@gmail.com>
2025-07-31 14:34:44 +0000

    feat(tests): add storage class filter unit tests

--- internal/kubernetes/watcher_test.go
+++ internal/kubernetes/watcher_test.go
@@ -113,6 +113,71 @@ func TestWatcherLabelling(t *testing.T) {
 	})
 }
 
+func TestWatcherStorageClassFilter(t *testing.T) {
+
+	t.Run("successful skipping ofunconfigured storage classes", func(t *testing.T) {
+		// create fake client
+		kube := testInternal.NewFakeClient()
+
+		labels := map[string]string{"app.kubernetes.io/part-of": "kubeflow-profile"}
+		if namespaceErr := kube.CreateNamespace(context.TODO(), "test", labels); namespaceErr != nil {
+			t.Fatalf("Error injecting namespace add: %v", namespaceErr)
+		}
+
+		names := []string{"pvc1", "pvc2"}
+
+		// inject fake pvcs
+		for _, name := range names {
+			if _, pvcErr := kube.CreatePersistentVolumeClaim(context.TODO(), name, "test"); pvcErr != nil {
+				t.Fatalf("Error injecting pvc add: %v", pvcErr)
+			}
+		}
+
+		ctx := context.Background()
+
+		cfg := structInternal.ControllerConfig{
+			Namespace:    "test",
+			TimeLabel:    "volume-cleaner/unattached-time",
+			NotifLabel:   "volume-cleaner/notification-count",
+			TimeFormat:   "2006-01-02_15-04-05Z",
+			StorageClass: []string{"non-existent-storage-class"},
+		}
+
+		go WatchSts(ctx, kube, cfg)
+
+		// mock a stateful set attached to a pvc1
+		if stsErr := kube.CreateStatefulSetWithPvc(context.TODO(), "sts1", "test", "pvc1"); stsErr != nil {
+			t.Fatalf("Error injecting sts add: %v", stsErr)
+		}
+
+		// delete sts
+		if eventErr := kube.DeleteStatefulSet(context.TODO(), "sts1", "test"); eventErr != nil {
+			t.Fatalf("Error injecting event add: %v", eventErr)
+		}
+
+		time.Sleep(2 * time.Second)
+
+		// should not have new labels
+
+		pvcs := PvcList(kube, "test")
+
+		_, ok := pvcs[0].Labels["volume-cleaner/unattached-time"]
+		assert.Equal(t, ok, false)
+
+		_, ok = pvcs[0].Labels["volume-cleaner/notification-count"]
+		assert.Equal(t, ok, false)
+
+		_, ok = pvcs[1].Labels["volume-cleaner/unattached-time"]
+		assert.Equal(t, ok, false)
+
+		_, ok = pvcs[1].Labels["volume-cleaner/notification-count"]
+		assert.Equal(t, ok, false)
+
+		ctx.Done()
+
+	})
+}
+
 func TestInitialScan(t *testing.T) {
 
 	t.Run("successful labelling of unatatched pvcs on controller startup", func(t *testing.T) {
